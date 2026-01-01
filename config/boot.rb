40242dfd2721a49fbe10e1a4ad9da8dbda32da8e
宮崎龍 <the.shrine.dragon@gmail.com>
2026-01-01 23:04:38 +0900

    「Ruby 3.2の互換性」「Babelのパッケージ不足」「Node.jsの暗号化規格」3つのエラーの解消

--- config/boot.rb
+++ config/boot.rb
@@ -1,9 +1,8 @@
+require "logger"
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
-require 'logger'
-
 require "yaml"
 module YAML
   class << self
