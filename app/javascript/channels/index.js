458014372982924ca209cb7fd7cbf8cb202f52e7
宮崎龍 <the.shrine.dragon@gmail.com>
2026-01-06 00:21:57 +0900

    faqの実装

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,5 +1,16 @@
-// Load all the channels within this directory and all subdirectories.
-// Channel files must be named *_channel.js.
+import $ from 'jquery';
 
-const channels = require.context('.', true, /_channel\.js$/)
-channels.keys().forEach(channels)
+$(document).on('turbolinks:load', function() { 
+  $('.question-and-answer').off('click').on('click', function(){
+    var $answer = $(this).find('.answer');
+    if($answer.hasClass('open')){
+      $answer.removeClass('open');
+      $answer.slideUp();
+      $(this).find('span').text('+');
+    } else {
+      $answer.addClass('open');
+      $answer.slideDown();
+      $(this).find('span').text('-');
+    }
+  });
+});
