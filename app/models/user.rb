9b54b6b4d71ea0eb8735211946ba3fad7b207145
宮崎龍 <the.shrine.dragon@gmail.com>
2026-01-15 05:54:26 +0900

    ActiveHash用のモデルを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  extend ActiveHash::Associations::ActiveRecordExtensions
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  belongs_to_active_hash :gender
 end
