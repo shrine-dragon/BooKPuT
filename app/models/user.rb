a7c1a9bb0e7e6f9ae946ec0fee6498362a503444
宮崎龍 <the.shrine.dragon@gmail.com>
2026-01-15 07:24:05 +0900

    画像保存に必要なgemの導入&設定を付与

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one_attached :image
   belongs_to_active_hash :gender
 end
