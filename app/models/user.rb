07594f9d7ddd94ec438e03cdc999f684ad923442
宮崎龍 <the.shrine.dragon@gmail.com>
2026-01-16 14:11:27 +0900

    エラーメッセージの日本語表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,12 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname, length: { minimum: 8, maximum: 16 }
     validates :birth_date
-    validates :email,    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: 'は@を含めてください'}
     validates :password, length: { minimum: 8, maximum: 20 },
-                         format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英数混合で入力してください' }
+                         format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英数混合で入力してください' },
+                         allow_blank: true
+    validates :password_confirmation
   end
   
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: 'は不正な形式です' }, allow_blank: true
   validates :gender_id, numericality: { other_than: 0, message: 'を選択してください' }
 end
