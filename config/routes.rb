68a5e4a6d114857cc89d0bedf75bb6bf89a9ad68
宮崎龍 <the.shrine.dragon@gmail.com>
2026-01-23 22:49:16 +0900

    ngrokの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/privacy_policy'
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
   root to: "books#index"
+  get 'privacy_policy', to: 'static_pages#privacy_policy'
 end
