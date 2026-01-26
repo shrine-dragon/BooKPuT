3137b1eaa23fe8715ce2592066bd3aa7685b3327
宮崎龍 <the.shrine.dragon@gmail.com>
2026-01-26 11:22:35 +0900

    ログインの有無で条件分岐&トップページの表記を変更

--- app/javascript/channels/header.js
+++ app/javascript/channels/header.js
@@ -3,21 +3,21 @@ import $ from 'jquery';
 const loadEvent = (typeof Turbo !== 'undefined') ? 'turbo:load' : 'turbolinks:load';
 
 $(document).on(loadEvent, function() {
-  $('.sign-up-btn').on('mouseover', function(){
+  $('.sign-up-hover').on('mouseover', function(){
     $("#modal-overlay").stop(true, true).fadeIn(200); // 背景を暗くする
     $(".modal.sign-up").stop(true, true).css("display", "flex").hide().fadeIn(200);
     $(".modal.log-in").hide(); // ログインが開いていれば隠す
   });
 
-  $('.log-in-btn').on('mouseover', function(){
+  $('.log-in-hover').on('mouseover', function(){
     $("#modal-overlay").stop(true, true).fadeIn(200);
     $(".modal.log-in").stop(true, true).css("display", "flex").hide().fadeIn(200);
     $(".modal.sign-up").hide(); // 新規登録が開いていれば隠す
   });
 
   $(document).on('mouseover', function(e) {
-    const isInsideSignUp = $(e.target).closest('.modal.sign-up, .sign-up-btn').length;
-    const isInsideLogIn = $(e.target).closest('.modal.log-in, .log-in-btn').length;
+    const isInsideSignUp = $(e.target).closest('.modal.sign-up, .sign-up-hover').length;
+    const isInsideLogIn = $(e.target).closest('.modal.log-in, .log-in-hover').length;
 
     // どちらのエリアにもマウスが乗っていない場合のみ消す
     if (!isInsideSignUp && !isInsideLogIn) {
