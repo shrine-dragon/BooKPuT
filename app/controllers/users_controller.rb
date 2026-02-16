b1feba97c9440b19862856e3cf52bff6975fe7fd
宮崎龍 <the.shrine.dragon@gmail.com>
2026-02-17 08:47:19 +0900

    パスワード変更ページのレイアウトを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,21 @@ class UsersController < ApplicationController
   end
 
   def update
-    if @user.update(user_params)
-      redirect_to user_path(@user), notice: '更新しました'
+    # パスワードの入力があるかどうかで処理を分ける
+    if params[:user][:password].blank?
+      # パスワードが空の場合、パスワード以外の項目を更新する
+      if @user.update_without_password(user_params)
+        redirect_to user_path(@user), notice: '更新しました'
+      else
+        render :edit
+      end
     else
-      render :edit
+      # パスワードの入力がある場合は、通常通りバリデーションを通して更新
+      if @user.update(user_params)
+        redirect_to user_path(@user), notice: '更新しました'
+      else
+        render :edit
+      end
     end
   end
 
@@ -25,7 +36,10 @@ class UsersController < ApplicationController
   private
   # ストロングパラメーター
   def user_params
-    params.require(:user).permit(:nickname, :image, :birth_date, :gender_id, :email)
+    params.require(:user).permit(
+      :nickname, :image, :birth_date, :gender_id, :email, 
+      :password, :password_confirmation
+    )
   end
 
   def set_user
