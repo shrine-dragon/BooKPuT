252c97e4cb2de58e662716194811096dbcd3519c
宮崎龍 <the.shrine.dragon@gmail.com>
2026-02-26 02:35:56 +0900

    application.scss内のコードを別ファイルに分別化

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2026_02_25_083658) do
+ActiveRecord::Schema.define(version: 2026_02_25_130849) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 2026_02_25_083658) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "book_contents", force: :cascade do |t|
+    t.text "content", null: false
+    t.integer "book_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_book_contents_on_book_id"
   end
 
   create_table "books", force: :cascade do |t|
@@ -66,5 +74,6 @@ ActiveRecord::Schema.define(version: 2026_02_25_083658) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "book_contents", "books"
   add_foreign_key "sns_credentials", "users"
 end
