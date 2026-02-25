047f943e00f9042af82a26701db67a0095e82350
宮崎龍 <the.shrine.dragon@gmail.com>
2026-02-25 17:36:27 +0900

    booksコントローラーの追記

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
-  before_action :set_user
+  before_action :authenticate_user!, except: %i[index show]
+  before_action :set_user, only: %i[show edit update destroy]
   before_action :ensure_correct_user, only: %i[edit update destroy]
 
   def index; end
@@ -16,11 +17,16 @@ class BooksController < ApplicationController
 
   def destroy; end
 
+  private
+
+  def book_params
+  end
+
   def set_book
     @book = Book.find(params[:id])
   end
 
   def ensure_correct_user
-    redirect_to root_path if current_user != @user
+    redirect_to root_path if current_user != @book.user
   end
 end
