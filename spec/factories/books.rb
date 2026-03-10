efea58580ca0c21e30fb04ae61f7ad93dc2c75b0
宮崎龍 <the.shrine.dragon@gmail.com>
2026-03-10 11:43:18 +0900

    テストコード用のダミーデータの作成

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :book do
-    
+    title       { Faker::Book.title }
+    category_id { Faker::Number.between(from: 1, to: 12) }
+
+    association :user
+
+    after(:build) do |book|
+      book.book_contents << build(:book_content, book: book)
+    end
+
+    after(:build) do |book|
+      book.image.attach(io: File.open('app/assets/images/Doflamingo.png'), filename: 'Doflamingo.png', content_type: 'image/png')
+    end
   end
 end
