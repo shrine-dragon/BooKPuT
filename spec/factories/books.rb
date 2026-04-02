9d346209db9347794563c2c3492838b9785770fa
宮崎龍 <the.shrine.dragon@gmail.com>
2026-04-02 16:55:59 +0900

    本のジャンルが追記された単体テストコードの実装

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -3,6 +3,8 @@ FactoryBot.define do
     title       { Faker::Book.title }
     category_id { Faker::Number.between(from: 1, to: 12) }
 
+    genre_ids   { [Faker::Number.between(from: 1, to: 89)] }
+
     association :user
 
     after(:build) do |book|
