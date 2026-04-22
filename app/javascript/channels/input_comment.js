1672a07ea0334b478657e277ec29fa4c4380a6a3
宮崎龍 <the.shrine.dragon@gmail.com>
2026-04-22 23:39:58 +0900

    コメントフォームと送信ボタンを作成

---
+++ app/javascript/channels/input_comment.js
@@ -0,0 +1,15 @@
+document.addEventListener('turbolinks:load', () => {
+  const commentFormText = document.querySelector('.comment-form-text');
+  const commentBtn = document.querySelector('.comment-btn');
+
+  if(!commentFormText) return;
+
+  commentFormText.addEventListener('input', () => {
+    const text_count = commentFormText.value.length;
+    if (text_count == 0){
+      commentBtn.classList.remove('is-show');
+    } else {
+      commentBtn.classList.add('is-show');
+    }
+  });
+});
