05bc08b77e9964276ad2a3d77498a2e1fbf6ed55
宮崎龍 <the.shrine.dragon@gmail.com>
2026-05-22 00:10:05 +0900

    コメント機能における結合テストコードのリファクタリング

--- spec/support/comment_support.rb
+++ spec/support/comment_support.rb
@@ -1,15 +1,6 @@
 module CommentSupport
   extend ActiveSupport::Concern
 
-  included do
-    let(:user1)   { FactoryBot.create(:user) }
-    let(:user2)   { FactoryBot.create(:user) }
-    let(:user3)   { FactoryBot.create(:user) }
-
-    let!(:book)   { FactoryBot.create(:book, user: user1) }
-    let(:comment) { FactoryBot.create(:comment, user: user2, book: book) }
-  end
-
   def check_comment_info
     new_comment = Comment.last
 
