bc0e45b178b30464f1a671e2ccd60e4948152774
宮崎龍 <the.shrine.dragon@gmail.com>
2026-05-31 10:13:41 +0900

    ビューファイルの編集・作成

--- config/routes.rb
+++ config/routes.rb
@@ -40,7 +40,8 @@ Rails.application.routes.draw do
     member do
       post 'report'
     end
-    resources :book_goods, only: [:create, :destroy]
+    post 'book_goods', to: 'book_goods#create', as: :goods
+    delete 'book_goods', to: 'book_goods#destroy', as: :good
     
     resources :comments, only: %i[create edit update destroy] do
       member do
