ebfc770446c64f2961e9148052bd9ad628080711
宮崎龍 <the.shrine.dragon@gmail.com>
2026-06-03 21:33:56 +0900

    コントローラーの追記・修正

--- app/controllers/comment_bads_controller.rb
+++ app/controllers/comment_bads_controller.rb
@@ -1,14 +1,13 @@
 class CommentBadsController < ApplicationController
   before_action :authenticate_user! 
   before_action :set_comment
+  before_action :set_book
 
   def create
     return if @comment.user_id == current_user.id
 
     current_user.comment_goods.find_by(comment_id: @comment.id)&.destroy
 
-    @book = @comment.book
-    
     @comment_bad = CommentBad.create(user_id: current_user.id, comment_id: @comment.id)
     
     respond_to do |format|
@@ -18,8 +17,6 @@ class CommentBadsController < ApplicationController
 
   def destroy
     return if @comment.user_id == current_user.id
-
-    @book = @comment.book
 
     @comment_bad = CommentBad.find_by(user_id: current_user.id, comment_id: @comment.id)
     @comment_bad.destroy
@@ -34,5 +31,9 @@ class CommentBadsController < ApplicationController
   def set_comment
     comment_id = params[:comment_id] || params[:id]
     @comment = Comment.find(comment_id)
+  end
+
+  def set_book
+    @book = @comment.book
   end
 end
