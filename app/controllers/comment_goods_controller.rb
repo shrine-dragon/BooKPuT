41830657a3dff69afc6cd5cc59c4f176225b42cf
宮崎龍 <the.shrine.dragon@gmail.com>
2026-06-03 20:41:15 +0900

    コメント高評価機能におけるデバッグ&低評価機能のコードの一時的な削除

--- app/controllers/comment_goods_controller.rb
+++ app/controllers/comment_goods_controller.rb
@@ -1,18 +1,28 @@
 class CommentGoodsController < ApplicationController
+  before_action :authenticate_user! 
   before_action :set_comment
-  before_action :ensure_correct_user
 
   def create
-    current_user.comment_bads.find_by(comment_id: @comment.id)&.destroy
+    return if @comment.user_id == current_user.id
+
+    # current_user.comment_bads.find_by(comment_id: @comment.id)&.destroy
+
+    @book = @comment.book
+    
     @comment_good = CommentGood.create(user_id: current_user.id, comment_id: @comment.id)
+    
     respond_to do |format|
       format.js
     end
   end
 
   def destroy
+    return if @comment.user_id == current_user.id
+
+    @book = @comment.book
+
     @comment_good = CommentGood.find_by(user_id: current_user.id, comment_id: @comment.id)
-    @comment_good.destroy
+    
     respond_to do |format|
       format.js
     end
@@ -21,10 +31,7 @@ class CommentGoodsController < ApplicationController
   private
 
   def set_comment
-    @comment = Comment.find(params[:comment_id])
-  end
-
-  def ensure_correct_user
-    redirect_to root_path if @comment.user_id == current_user.id
+    comment_id = params[:comment_id] || params[:id]
+    @comment = Comment.find(comment_id)
   end
 end
--- db/migrate/20260603052037_create_comment_goods.rb
+++ db/migrate/20260603052037_create_comment_goods.rb
@@ -1,7 +1,7 @@
 class CreateCommentGoods < ActiveRecord::Migration[6.0]
   def change
     create_table :comment_goods do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :user,    null: false, foreign_key: true
       t.references :comment, null: false, foreign_key: true
       t.index [:user_id, :comment_id], unique: true
       t.timestamps
