dc822b3901d05a9cb97e693d12325ba0d4ac8203
宮崎龍 <the.shrine.dragon@gmail.com>
2026-06-03 21:40:24 +0900

    コメント低評価用のビューファイルの作成

--- app/controllers/book_goods_controller.rb
+++ app/controllers/book_goods_controller.rb
@@ -1,18 +1,26 @@
 class BookGoodsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_book
-  before_action :ensure_correct_user
 
   def create
+    return if @book.user_id == current_user.id
+
     current_user.book_bads.find_by(book_id: @book.id)&.destroy
+
     @book_good = BookGood.create(user_id: current_user.id, book_id: @book.id)
+
     respond_to do |format|
       format.js
     end
   end
 
   def destroy
+    return if @book.user_id == current_user.id
+
     @book_good = BookGood.find_by(user_id: current_user.id, book_id: @book.id)
+
     @book_good.destroy
+
     respond_to do |format|
       format.js
     end
@@ -22,9 +30,5 @@ class BookGoodsController < ApplicationController
 
   def set_book
     @book = Book.find(params[:book_id])
-  end
-
-  def ensure_correct_user
-    redirect_to root_path if @book.user_id == current_user.id
   end
 end
